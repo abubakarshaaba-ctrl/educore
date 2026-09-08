9c71d9bad4d8c592d7e05080053838e7a5215b71
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-08 20:08:20 +0100

    Expose native report card computation

--- educore/routes/mobile-reports.php
+++ educore/routes/mobile-reports.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('reports')->group(function (): void {
     Route::get('/', [MobileReportsController::class, 'index']);
+    Route::post('compute', [MobileReportsController::class, 'compute']);
     Route::post('publish', [MobileReportsController::class, 'publish']);
     Route::post('unpublish', [MobileReportsController::class, 'unpublish']);
 });
