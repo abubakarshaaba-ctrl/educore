c3c98c740698a7eecaa585e6f2209669a536ab29
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-08 21:16:11 +0100

    Add native Platform support and broadcast mutation API

--- mobile-native/core/network/src/main/java/online/educoreng/educore/core/network/PlatformApi.kt
+++ mobile-native/core/network/src/main/java/online/educoreng/educore/core/network/PlatformApi.kt
@@ -3,15 +3,21 @@ package online.educoreng.educore.core.network
 import online.educoreng.educore.core.network.dto.PlatformAgentsDto
 import online.educoreng.educore.core.network.dto.PlatformAnalyticsDto
 import online.educoreng.educore.core.network.dto.PlatformBillingDto
+import online.educoreng.educore.core.network.dto.PlatformBroadcastCreateRequestDto
 import online.educoreng.educore.core.network.dto.PlatformBroadcastsDto
 import online.educoreng.educore.core.network.dto.PlatformDashboardDto
 import online.educoreng.educore.core.network.dto.PlatformGatewaysDto
 import online.educoreng.educore.core.network.dto.PlatformGroupsDto
+import online.educoreng.educore.core.network.dto.PlatformMutationResponseDto
 import online.educoreng.educore.core.network.dto.PlatformPlansDto
 import online.educoreng.educore.core.network.dto.PlatformSettingsDto
 import online.educoreng.educore.core.network.dto.PlatformSupportDto
+import online.educoreng.educore.core.network.dto.PlatformSupportReplyRequestDto
 import online.educoreng.educore.core.network.dto.PlatformTenantsDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PlatformApi {
@@ -42,8 +48,23 @@ interface PlatformApi {
     @GET("platform/support")
     suspend fun support(): PlatformSupportDto
 
+    @POST("platform/support/{ticket}/reply")
+    suspend fun replySupport(
+        @Path("ticket") ticket: Long,
+        @Body body: PlatformSupportReplyRequestDto,
+    ): PlatformMutationResponseDto
+
+    @POST("platform/support/{ticket}/close")
+    suspend fun closeSupport(@Path("ticket") ticket: Long): PlatformMutationResponseDto
+
     @GET("platform/broadcasts")
     suspend fun broadcasts(): PlatformBroadcastsDto
+
+    @POST("platform/broadcasts")
+    suspend fun createBroadcast(@Body body: PlatformBroadcastCreateRequestDto): PlatformMutationResponseDto
+
+    @POST("platform/broadcasts/{broadcast}/expire")
+    suspend fun expireBroadcast(@Path("broadcast") broadcast: Long): PlatformMutationResponseDto
 
     @GET("platform/settings")
     suspend fun settings(): PlatformSettingsDto
