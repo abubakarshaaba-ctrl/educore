3de3025adfbdbdbbd22ef6a6decff94b4036a121
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-08 12:05:16 +0100

    Route native exports through operations endpoint

--- educore/app/Http/Controllers/Api/MobileOperationsController.php
+++ educore/app/Http/Controllers/Api/MobileOperationsController.php
@@ -4,18 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\Mobile\MobileOperationsService;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MobileOperationsController extends Controller
 {
-    public function show(Request $request, string $module, MobileOperationsService $operations): JsonResponse
+    public function show(Request $request, string $module, MobileOperationsService $operations): Response
     {
         $user = $request->user();
         abort_unless($user, 401);
 
         if ($module === 'analytics') {
             return app(MobileAnalyticsController::class)($request);
+        }
+
+        if ($module === 'exports') {
+            return app(MobileExportsController::class)($request);
         }
 
         return response()->json($operations->for($user, $module));
