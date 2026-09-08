b45505dcf0623c0f951c1f90d76e21236aad14bb
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-08 19:58:15 +0100

    Route native report cards API

---
+++ educore/routes/mobile-reports.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Http\Controllers\Api\MobileReportsController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('reports')->group(function (): void {
+    Route::get('/', [MobileReportsController::class, 'index']);
+    Route::post('publish', [MobileReportsController::class, 'publish']);
+    Route::post('unpublish', [MobileReportsController::class, 'unpublish']);
+});
