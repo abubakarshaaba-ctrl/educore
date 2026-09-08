a2d0348a9d34be19f608d9b2ed1652d0f99dd0f5
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-08 17:27:39 +0100

    Expose permissioned native interclass transfer endpoints

--- educore/routes/mobile-transfers.php
+++ educore/routes/mobile-transfers.php
@@ -5,9 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('transfers')->group(function (): void {
     Route::get('/', [MobileTransfersController::class, 'index']);
+
     Route::post('cross-school', [MobileTransfersController::class, 'requestCrossSchool']);
     Route::post('cross-school/{transfer}/approve', [MobileTransfersController::class, 'approveCrossSchool'])
         ->whereNumber('transfer');
     Route::post('cross-school/{transfer}/reject', [MobileTransfersController::class, 'rejectCrossSchool'])
+        ->whereNumber('transfer');
+
+    Route::post('interclass', [MobileTransfersController::class, 'requestInterclass']);
+    Route::post('interclass/{transfer}/approve', [MobileTransfersController::class, 'approveInterclass'])
+        ->whereNumber('transfer');
+    Route::post('interclass/{transfer}/reject', [MobileTransfersController::class, 'rejectInterclass'])
+        ->whereNumber('transfer');
+    Route::post('interclass/{transfer}/cancel', [MobileTransfersController::class, 'cancelInterclass'])
         ->whereNumber('transfer');
 });
