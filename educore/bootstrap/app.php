101f32158044c6dc34cdf1762deb0b74ad136aea
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-08 16:14:00 +0100

    Enforce public tenant portal security middleware

--- educore/bootstrap/app.php
+++ educore/bootstrap/app.php
@@ -48,6 +48,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\MaintenanceMode::class,
             \App\Http\Middleware\RestrictCbtLanSession::class,
+            \App\Http\Middleware\ProtectPublicTenantPortal::class,
         ]);
 
         $middleware->alias([
