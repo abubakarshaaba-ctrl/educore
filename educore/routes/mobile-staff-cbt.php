9b851e518059376155aa0a5ef874468fc3d38761
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-09 20:18:53 +0100

    Restore full native staff CBT route lifecycle

--- educore/routes/mobile-staff-cbt.php
+++ educore/routes/mobile-staff-cbt.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\StaffCbtApiController;
 use App\Http\Controllers\Api\StaffCbtCreateApiController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,3 +8,8 @@ use Illuminate\Support\Facades\Route;
 // token middleware used by the primary mobile API route file.
 Route::get('options', [StaffCbtCreateApiController::class, 'options']);
 Route::post('exams', [StaffCbtCreateApiController::class, 'store']);
+Route::get('exams', [StaffCbtApiController::class, 'index']);
+Route::get('exams/{exam}', [StaffCbtApiController::class, 'show'])->whereNumber('exam');
+Route::post('exams/{exam}/publish', [StaffCbtApiController::class, 'publish'])->whereNumber('exam');
+Route::post('exams/{exam}/close', [StaffCbtApiController::class, 'close'])->whereNumber('exam');
+Route::patch('exams/{exam}/schedule', [StaffCbtApiController::class, 'reschedule'])->whereNumber('exam');
