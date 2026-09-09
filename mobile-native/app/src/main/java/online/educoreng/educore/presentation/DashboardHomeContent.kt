089cf2a423a3f637e808b3f9c721bb2975968591
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-09 13:48:55 +0100

    Fix dashboard grid items import

--- mobile-native/app/src/main/java/online/educoreng/educore/presentation/DashboardHomeContent.kt
+++ mobile-native/app/src/main/java/online/educoreng/educore/presentation/DashboardHomeContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
