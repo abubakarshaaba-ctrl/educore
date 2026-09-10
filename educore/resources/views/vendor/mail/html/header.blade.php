e6a65eda72ef78cc783c59ffd7a96ac11ee9d639
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-10 18:31:07 +0100

    Brand all outgoing email headers

---
+++ educore/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display:inline-block;text-decoration:none;">
+    <img src="{{ rtrim(config('app.url'), '/') }}/brand/educore-logo-horizontal-dark.svg" class="brand-logo" alt="EduCore" width="190">
+    <span class="brand-fallback" aria-hidden="true"><span class="brand-edu">Edu</span><span class="brand-core">Core</span></span>
+</a>
+<div class="brand-tagline">School Management &amp; Learning Platform</div>
+</td>
+</tr>
