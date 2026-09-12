c7de396820094eabda08a313541da48f36a000da
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-12 10:33:04 +0100

    Apply strict web RBAC and school week middleware

--- educore/bootstrap/app.php
+++ educore/bootstrap/app.php
@@ -85,6 +85,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\MaintenanceMode::class,
             \App\Http\Middleware\RestrictCbtLanSession::class,
+            \App\Http\Middleware\EnforceStrictWebRbac::class,
+            \App\Http\Middleware\EnforceSchoolOpenDay::class,
         ]);
 
         $middleware->alias([
