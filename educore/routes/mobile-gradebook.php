33a1c59b219bf946cd18dd37df8e6421d7d2c144
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-12 07:20:37 +0100

    Add native gradebook API routes

---
+++ educore/routes/mobile-gradebook.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Api\MobileGradebookController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('gradebook')->group(function (): void {
+    Route::get('/', [MobileGradebookController::class, 'index']);
+    Route::put('remarks/{summary}', [MobileGradebookController::class, 'updateRemark'])->whereNumber('summary');
+});
