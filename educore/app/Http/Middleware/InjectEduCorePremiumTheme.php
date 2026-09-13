4d361904efbea1f2ce0beb3ba69e923c2c7a6472
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-13 20:38:33 +0100

    style: bump premium theme cache version

--- educore/app/Http/Middleware/InjectEduCorePremiumTheme.php
+++ educore/app/Http/Middleware/InjectEduCorePremiumTheme.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class InjectEduCorePremiumTheme
 {
-    private const THEME_VERSION = '20260913-premium-v3';
+    private const THEME_VERSION = '20260913-premium-v4';
 
     public function handle(Request $request, Closure $next): Response
     {
