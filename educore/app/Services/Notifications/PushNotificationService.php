2092dad89b26dcbec79760d469897c2080ee56e8
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-13 14:09:54 +0100

    fix(push): restore Firebase OAuth grant type

--- educore/app/Services/Notifications/PushNotificationService.php
+++ educore/app/Services/Notifications/PushNotificationService.php
@@ -279,7 +279,7 @@ class PushNotificationService
             $response = Http::timeout(15)
                 ->asForm()
                 ->post('https://oauth2.googleapis.com/token', [
-                    'grant_type' => 'urn:ietf:params:oauth-grant-type:jwt-bearer',
+                    'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
                     'assertion' => $jwt,
                 ]);
 
