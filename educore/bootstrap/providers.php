410eb9d5a0e022ded4d77721374c4da589e6e7d1
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-14 02:08:34 +0100

    Enable mobile Academic Repository knowledge API

--- educore/bootstrap/providers.php
+++ educore/bootstrap/providers.php
@@ -4,12 +4,14 @@ use App\Providers\AcademicRepositoryKnowledgeServiceProvider;
 use App\Providers\AppServiceProvider;
 use App\Providers\AssessmentConfigurationServiceProvider;
 use App\Providers\DataMigrationServiceProvider;
+use App\Providers\MobileAcademicKnowledgeApiServiceProvider;
 use App\Providers\MobileSubscriptionApiServiceProvider;
 
 return [
     AppServiceProvider::class,
     AssessmentConfigurationServiceProvider::class,
     AcademicRepositoryKnowledgeServiceProvider::class,
+    MobileAcademicKnowledgeApiServiceProvider::class,
     MobileSubscriptionApiServiceProvider::class,
     DataMigrationServiceProvider::class,
 ];
