778320a21a552799ec7b50b83b644e185082ffa3
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-14 13:24:29 +0100

    build(android): bump release after R8 network fix

--- mobile-native/app/build.gradle.kts
+++ mobile-native/app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "online.educoreng.educore"
         minSdk = 23
         targetSdk = 36
-        versionCode = 16
-        versionName = "2.0.0-alpha03"
+        versionCode = 17
+        versionName = "2.0.0-alpha04"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
