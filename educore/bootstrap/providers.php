e7074b6987e17f6acfd54a04e67e76d8d35299d6
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-14 05:54:28 +0100

    Register mobile dashboard presentation provider

--- educore/bootstrap/providers.php
+++ educore/bootstrap/providers.php
@@ -2,10 +2,12 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\DataMigrationServiceProvider;
+use App\Providers\MobileDashboardPresentationServiceProvider;
 use App\Providers\MobileOperationsApiServiceProvider;
 
 return [
     AppServiceProvider::class,
     MobileOperationsApiServiceProvider::class,
+    MobileDashboardPresentationServiceProvider::class,
     DataMigrationServiceProvider::class,
 ];
