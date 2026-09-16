1aeac60e665b547c48641a1052d062b0e731fc9d
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-16 18:38:54 +0100

    Add staff profile submission model

---
+++ educore/app/Models/StaffProfileSubmission.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+class StaffProfileSubmission extends BaseTenantModel
+{
+    protected $fillable = [
+        'tenant_id','name','email','phone','date_of_birth','gender','qualification','address',
+        'employment_started_at','position_title','department_name','employment_type','functional_role',
+        'grade_level','appointment_type','password_hash','status','reviewed_by','reviewed_at',
+    ];
+
+    protected $hidden = ['password_hash'];
+
+    protected function casts(): array
+    {
+        return [
+            'date_of_birth' => 'date',
+            'employment_started_at' => 'date',
+            'reviewed_at' => 'datetime',
+        ];
+    }
+
+    public function reviewer()
+    {
+        return $this->belongsTo(User::class, 'reviewed_by');
+    }
+}
