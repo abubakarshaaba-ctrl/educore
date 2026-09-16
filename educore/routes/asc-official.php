6bb3e990b3c91eba401942ab4a9214e12a1c3462
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-16 18:03:27 +0100

    Add ASC PDF export route

--- educore/routes/asc-official.php
+++ educore/routes/asc-official.php
@@ -1,12 +1,16 @@
 <?php
 
 use App\Http\Controllers\AscDerivedController;
+use App\Http\Controllers\AscExportController;
 use App\Http\Controllers\AscReviewController;
 use App\Http\Controllers\AscSectionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('asc/review', [AscReviewController::class, 'index'])
     ->name('asc.review');
+
+Route::get('asc/export/pdf', [AscExportController::class, 'pdf'])
+    ->name('asc.export.pdf');
 
 Route::prefix('asc/section')->name('asc.section.')->group(function () {
     Route::get('{section}', [AscSectionController::class, 'show'])
