eab21b1b900899f8a591940d104ee7e3146603b3
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-16 18:17:51 +0100

    Add standardized staff highest qualification options

---
+++ educore/config/staff.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'highest_qualifications' => [
+        'SSCE',
+        'NCE',
+        'ND',
+        'HND',
+        'BSc',
+        'BEd',
+        'BSc/PDE',
+        'BSc/PGDE',
+        'BEng',
+        'BEng/PGDE',
+        'BEng/PDE',
+        'BTech',
+        'BTech/PDE',
+        'BTech/PGDE',
+        'BA',
+        'BA/PDE',
+        'BA/PGDE',
+        'MSc',
+        'MA',
+        'MSc/PDE',
+        'MSc/PGDE',
+        'MA/PDE',
+        'MA/PGDE',
+    ],
+];
