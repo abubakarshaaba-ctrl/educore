0a380a4b25fe7776c8652371240a4584247047f8
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-17 21:08:00 +0100

    Expose latest Android release metadata

--- educore/routes/mobile-release.php
+++ educore/routes/mobile-release.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\Api\MobileReleaseController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('mobile-release/latest', [MobileReleaseController::class, 'latest'])
+    ->middleware('throttle:60,1')
+    ->name('api.mobile-release.latest');
+
 Route::post('mobile-release/notify', MobileReleaseController::class)
     ->middleware('throttle:6,1')
     ->name('api.mobile-release.notify');
