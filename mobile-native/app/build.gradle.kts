ada85f4b2ab9fbbd28e304c4dc315c303c13a9dc
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-18 09:06:44 +0100

    Bump native Android release to versionCode 22

--- mobile-native/app/build.gradle.kts
+++ mobile-native/app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "online.educoreng.educore"
         minSdk = 23
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.0.0-alpha08"
+        versionCode = 22
+        versionName = "2.0.0-alpha09"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
