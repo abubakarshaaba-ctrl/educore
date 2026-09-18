9118d2fa4e83eb0afb0f7f3a45b15ec0ba85ed73
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-18 04:41:13 +0100

    Prepare Android 2.0.0-alpha07 release

--- mobile-native/app/build.gradle.kts
+++ mobile-native/app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "online.educoreng.educore"
         minSdk = 23
         targetSdk = 36
-        versionCode = 19
-        versionName = "2.0.0-alpha06"
+        versionCode = 20
+        versionName = "2.0.0-alpha07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
