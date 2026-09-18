0588e53a75e4ef97ab43aff3c279c8879ced2ed1
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-18 15:22:33 +0100

    Unify parallel curriculum lifecycle navigation

--- educore/resources/views/parallel-curriculum/results/index.blade.php
+++ educore/resources/views/parallel-curriculum/results/index.blade.php
@@ -25,6 +25,7 @@
         <a href="{{ route('scores.index') }}" class="pc-tab">Conventional Scores</a>
         <a href="{{ route('parallel-curriculum.index') }}" class="pc-tab">Parallel Curriculum</a>
         <a href="{{ route('parallel-curriculum.student-assignments') }}" class="pc-tab">Student Assignments</a>
+        <a href="{{ route('parallel-curriculum.lifecycle.index') }}" class="pc-tab">Academic Lifecycle</a>
         <a href="{{ route('parallel-curriculum.results.index') }}" class="pc-tab active">Parallel Results</a>
     </div>
 
