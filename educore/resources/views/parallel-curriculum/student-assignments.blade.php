31cd92aef5a29e9c3b1f85140d632549727bcef0
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-18 13:54:58 +0100

    Align parallel curriculum UI with global EduCore theme

--- educore/resources/views/parallel-curriculum/student-assignments.blade.php
+++ educore/resources/views/parallel-curriculum/student-assignments.blade.php
@@ -20,6 +20,10 @@
 </style>
 @endpush
 
+@push('styles')
+@include('parallel-curriculum.partials.global-ui')
+@endpush
+
 @section('content')
 <div class="pc-assign">
     @if(session('success'))<div class="alert-s">{{ session('success') }}</div>@endif
