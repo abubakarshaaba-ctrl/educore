93873f0335590eb5508517b6539b7f0dfb3244d5
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-19 14:57:07 +0100

    Protect class-teacher mode from subject overrides

--- educore/app/Http/Controllers/Api/MobileParallelCurriculumLifecycleController.php
+++ educore/app/Http/Controllers/Api/MobileParallelCurriculumLifecycleController.php
@@ -1685,6 +1685,13 @@ class MobileParallelCurriculumLifecycleController extends Controller
         $arm = ParallelCurriculumClassArm::with('curriculumClass')
             ->findOrFail($data['parallel_curriculum_class_arm_id']);
         $class = $arm->curriculumClass;
+
+        abort_if(
+            $arm->usesClassTeacherModel(),
+            422,
+            'Switch this class arm to subject-based teaching before assigning subject teachers.'
+        );
+
         $subject = ParallelCurriculumSubject::findOrFail(
             $data['parallel_curriculum_subject_id']
         );
