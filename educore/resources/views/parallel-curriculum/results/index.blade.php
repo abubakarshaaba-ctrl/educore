723fddd86d1ad7ea6775f5102725a4cd9c5882a2
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-20 08:38:31 +0100

    Add parallel class and teacher directory navigation

--- educore/resources/views/parallel-curriculum/results/index.blade.php
+++ educore/resources/views/parallel-curriculum/results/index.blade.php
@@ -26,6 +26,8 @@
         <a href="{{ route('parallel-curriculum.index') }}" class="pc-tab">Parallel Workspace</a>
         <a href="{{ route('parallel-curriculum.setup') }}" class="pc-tab">Programme Setup</a>
         <a href="{{ route('parallel-curriculum.student-assignments') }}" class="pc-tab">Student Assignments</a>
+        <a href="{{ route('parallel-curriculum.class-list') }}" class="pc-tab">Class List</a>
+        <a href="{{ route('parallel-curriculum.teacher-list') }}" class="pc-tab">Teacher List</a>
         <a href="{{ route('parallel-curriculum.lifecycle.index') }}" class="pc-tab">Academic Lifecycle</a>
         <a href="{{ route('parallel-curriculum.operations.index') }}" class="pc-tab">Timetable & Attendance</a>
         <a href="{{ route('parallel-curriculum.results.index') }}" class="pc-tab active">Parallel Results</a>
