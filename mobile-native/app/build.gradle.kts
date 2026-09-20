6ceaecfc5dc1037d858437c4b82543a781619614
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-20 18:16:38 +0100

    Bump Android release for native parity completion

--- mobile-native/app/build.gradle.kts
+++ mobile-native/app/build.gradle.kts
@@ -74,8 +74,8 @@ android {
         applicationId = "online.educoreng.educore"
         minSdk = 23
         targetSdk = 36
-        versionCode = 27
-        versionName = "2.0.0-alpha14"
+        versionCode = 28
+        versionName = "2.0.0-alpha15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
