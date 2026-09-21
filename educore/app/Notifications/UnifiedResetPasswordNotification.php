6eea41b60bb876b4a56e46aab869b33ad3764e17
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-21 04:10:31 +0100

    Use premium platform reset email view

--- educore/app/Notifications/UnifiedResetPasswordNotification.php
+++ educore/app/Notifications/UnifiedResetPasswordNotification.php
@@ -27,13 +27,15 @@ class UnifiedResetPasswordNotification extends Notification
         $expires = (int) config('auth.passwords.users.expire', 60);
 
         $mail = (new MailMessage)
-            ->subject('Reset your EduCore password')
-            ->greeting('Reset your password')
-            ->line('We received a request to reset the password for your EduCore account.')
-            ->action('Reset Password', $url)
-            ->line('For your security, this reset link expires in '.$expires.' minutes.')
-            ->line('If you did not request this change, ignore this email. Your current password remains unchanged.');
+            ->subject('Reset your EduCore password');
 
-        return MailBranding::platform($mail);
+        return MailBranding::platformView(
+            $mail,
+            'mail.platform.password-reset',
+            [
+                'resetUrl' => $url,
+                'expires' => $expires,
+            ],
+        );
     }
 }
