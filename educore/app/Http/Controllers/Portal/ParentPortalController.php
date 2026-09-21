7a7ed7432c9019e62606c8f5a19a7fc624ce3187
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-21 12:54:30 +0100

    Exclude platform broadcasts from parent portal

--- educore/app/Http/Controllers/Portal/ParentPortalController.php
+++ educore/app/Http/Controllers/Portal/ParentPortalController.php
@@ -96,6 +96,7 @@ class ParentPortalController extends Controller
 
         $announcements = Announcement::where('tenant_id', $guardian->tenant_id)
             ->where('is_published', true)
+            ->whereNull('platform_broadcast_id')
             ->whereIn('audience', ['all', 'parents'])
             ->latest('publish_date')->limit(6)->get();
 
@@ -339,6 +340,7 @@ class ParentPortalController extends Controller
 
         $announcements = Announcement::where('tenant_id', $guardian->tenant_id)
             ->where('is_published', true)
+            ->whereNull('platform_broadcast_id')
             ->whereIn('audience', ['all', 'parents'])
             ->latest('publish_date')->paginate(20);
 
