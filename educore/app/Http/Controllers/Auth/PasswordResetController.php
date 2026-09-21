fd1a9ae8fe0169f312429d4c19459557b7a62732
Haruna Abubakar <abubakarshaaba@gmail.com>
2026-09-21 03:24:48 +0100

    Use school branding for unified tenant resets

--- educore/app/Http/Controllers/Auth/PasswordResetController.php
+++ educore/app/Http/Controllers/Auth/PasswordResetController.php
@@ -39,7 +39,7 @@ class PasswordResetController extends Controller
 
         if ($user) {
             $token = Password::broker()->createToken($user);
-            $user->notify(new UnifiedResetPasswordNotification($token));
+            $user->notify(new UnifiedResetPasswordNotification($token, $user->tenant));
             $audit->recordForUser($user, 'auth.password_reset.requested', ['login_surface' => 'unified'], $request);
         }
 
